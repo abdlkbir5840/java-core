d0c2affb9bab48a2d2b87e43ca0f1845f482ef3d
abdlkbir5840 <habonbahmadi@gmail.com>
2024-02-09 11:37:56 +0100

    compare time for different operation of different map

--- collections/src/main/java/org/example/TestMap.java
+++ collections/src/main/java/org/example/TestMap.java
@@ -24,6 +24,65 @@ public class TestMap {
         testMap(notes2);
         System.out.println("------------------------ TreeMap ------------------------");
         testMap(notes3);
+        System.out.println();
+        System.out.println("============================ Add ==================================");
+        Map<Integer, Integer> numbers1 = new HashMap<>();
+        Map<Integer, Integer> numbers2 = new LinkedHashMap<>();
+        Map<Integer, Integer> numbers3 = new TreeMap<>();
+        long beforHMap, afterHMap,beforLHMap, afterLHMap, beforTMap, afterTMap;
+        beforHMap = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            numbers1.put(i,i);
+        }
+        afterHMap = System.currentTimeMillis();
+        System.out.println("HashMap: The time that has passed is: "+(afterHMap - beforHMap));
 
+        beforLHMap = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            numbers2.put(i,i);
+        }
+        afterLHMap = System.currentTimeMillis();
+        System.out.println("LinkedHashMap: The time that has passed is: "+(afterLHMap - beforLHMap));
+
+        beforTMap = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            numbers3.put(i,i);
+        }
+        afterTMap = System.currentTimeMillis();
+        System.out.println("TreeMap: The time that has passed is: "+(afterTMap - beforTMap));
+        System.out.println();
+        System.out.println("============================= Delete========================================");
+        System.out.println();
+        beforHMap = System.currentTimeMillis();
+        numbers1.remove(200);
+        afterHMap = System.currentTimeMillis();
+        System.out.println("HashMap: The time that has passed is: "+(afterHMap - beforHMap));
+
+        beforLHMap = System.currentTimeMillis();
+        numbers2.remove(200);
+        afterLHMap = System.currentTimeMillis();
+        System.out.println("LinkedHashMap: The time that has passed is: "+(afterLHMap - beforLHMap));
+
+        beforTMap = System.currentTimeMillis();
+        numbers3.remove(200);
+        afterTMap = System.currentTimeMillis();
+        System.out.println("TreeMap: The time that has passed is: "+(afterTMap - beforTMap));
+
+        System.out.println("============================= Get========================================");
+        System.out.println();
+        beforHMap = System.currentTimeMillis();
+        int value1= numbers1.get(300);
+        afterHMap = System.currentTimeMillis();
+        System.out.println("HashMap: The time that has passed for get value "+value1+" is: "+(afterHMap - beforHMap));
+
+        beforLHMap = System.currentTimeMillis();
+        int value2= numbers2.get(300);
+        afterLHMap = System.currentTimeMillis();
+        System.out.println("LinkedHashMap: The time that has passed for get value "+value2+" is: "+(afterLHMap - beforLHMap));
+
+        beforTMap = System.currentTimeMillis();
+        int value3= numbers3.get(300);
+        afterTMap = System.currentTimeMillis();
+        System.out.println("TreeMap: The time that has passed for get value "+value3+" is: "+(afterTMap - beforTMap));
     }
 }
